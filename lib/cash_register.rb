df131d998ea3bcd257d0374421b71918a6865b30
Victoria Hale <vic.hale74@gmail.com>
2019-10-18 01:55:02 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,7 +5,7 @@ class CashRegister
     @total = 0
     @discount = discount
   end
-  def add_item(title, price)
-    
+  def add_item(title, price, quantity= 1)
+    total += (price * quantity)
   end
 end
