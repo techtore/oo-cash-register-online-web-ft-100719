cbee2071fce35a9f0ff2a4b99a676f3e98f667c0
Victoria Hale <vic.hale74@gmail.com>
2019-10-18 02:14:20 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,12 +9,17 @@ class CashRegister
     
      @total += (price * quantity)
   end
+  
   def apply_discount
     if @discount != 0 
       @total = @total - (@total * (@discount / 100.00))
     return "After the discount, the total comes to $#{@total.to_i}."
-  else
-    "There is no discount to apply."
-  end
-  end
+    else
+      "There is no discount to apply."
+    end
+    end
+    
+  def items 
+    items = []
+  end 
 end
